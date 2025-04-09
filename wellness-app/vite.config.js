f5b754c772278f37b3f23fcc7f23e30611dee537
Martha Morufa <marthamarumo92@mail.com>
2025-04-09 20:51:45 +0200

    Removed the react-scripts

--- wellness-app/vite.config.js
+++ wellness-app/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  build: {
+    outDir: 'build', // Output directory for the build
+    assetsDir: 'assets', // Place your static assets in the "assets" folder
+  },
 });
